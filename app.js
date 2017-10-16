5ec6a3a52a959232764f2ca20c10dd1beb74fbfa
Ябуров Владимир Васильевич (СБТ) <VVYaburov.SBT@sberbank.ru>
2017-10-16 21:48:28 +0300

    added config

--- app.js
+++ app.js
@@ -1,55 +1,59 @@
 "use strict";
+
 var request = require("request");
+var config = require("config");
 var express = require("express");
 var app = express();
 
 
 app.get("/", function (req, res) {
-	const user = "user";
-	const pass = "pass";
-	const hostApi = "online.moysklad.ru/api/remap/1.1/";
+	const user = config.get("user");
+	const pass = config.get("pass");
+	const uriApi = config.get("uriApi");
 	const service = "report/stock/bystore?offset=0&limit=5";
-
 	
-	request({
-			uri:"https://" + user + ":" + pass + "@" + hostApi + service,
-			method: "GET"
-		},
-		function (error, response, body) {
-			if(error) {
-				console.log("error: " + error);
-				res.send("error: " + error);
-			} else {
-				res.type("json");
-				var json = JSON.parse(body);
-				
-				var a = json.rows.map(function (el) {
-					return {
-						id:   /\/([\w-]*)\?/g .exec(el.meta.href)[1],
-						uuid: /=([\w-]*)$/g   .exec(el.meta.uuidHref)[1],
-						href:                       el.meta.href,
-						
-						stock0Name:                       el.stockByStore[0].name,
-						stock0Id:     /\/([\w-]*)$/g.exec(el.stockByStore[0].meta.href)[1],
-						stock0Value:                      el.stockByStore[0].stock,
-						
-						stock1Name:                       el.stockByStore[1].name,
-						stock1Id:     /\/([\w-]*)$/g.exec(el.stockByStore[1].meta.href)[1],
-						stock1Value:                      el.stockByStore[1].stock,
-						
-						stock2Name:                       el.stockByStore[2].name,
-						stock2Id:     /\/([\w-]*)$/g.exec(el.stockByStore[2].meta.href)[1],
-						stock2Value:                      el.stockByStore[2].stock,
-						
-						stock3Name:                       el.stockByStore[3].name,
-						stock3Id:     /\/([\w-]*)$/g.exec(el.stockByStore[3].meta.href)[1],
-						stock3Value:                      el.stockByStore[3].stock
-					};
-				});
-				res.json(a);
-			}
-		}
-	);
+	console.log(pass);
+	res.json(process.env);
+	
+	// request({
+	// 		uri:"https://" + user + ":" + pass + "@" + uriApi + service,
+	// 		method: "GET"
+	// 	},
+	// 	function (error, response, body) {
+	// 		if(error) {
+	// 			console.log("error: " + error);
+	// 			res.send("error: " + error);
+	// 		} else {
+	// 			res.type("json");
+	// 			var json = JSON.parse(body);
+	//
+	// 			var a = json.rows.map(function (el) {
+	// 				return {
+	// 					id:   /\/([\w-]*)\?/g .exec(el.meta.href)[1],
+	// 					uuid: /=([\w-]*)$/g   .exec(el.meta.uuidHref)[1],
+	// 					href:                       el.meta.href,
+	//
+	// 					stock0Name:                       el.stockByStore[0].name,
+	// 					stock0Id:     /\/([\w-]*)$/g.exec(el.stockByStore[0].meta.href)[1],
+	// 					stock0Value:                      el.stockByStore[0].stock,
+	//
+	// 					stock1Name:                       el.stockByStore[1].name,
+	// 					stock1Id:     /\/([\w-]*)$/g.exec(el.stockByStore[1].meta.href)[1],
+	// 					stock1Value:                      el.stockByStore[1].stock,
+	//
+	// 					stock2Name:                       el.stockByStore[2].name,
+	// 					stock2Id:     /\/([\w-]*)$/g.exec(el.stockByStore[2].meta.href)[1],
+	// 					stock2Value:                      el.stockByStore[2].stock,
+	//
+	// 					stock3Name:                       el.stockByStore[3].name,
+	// 					stock3Id:     /\/([\w-]*)$/g.exec(el.stockByStore[3].meta.href)[1],
+	// 					stock3Value:                      el.stockByStore[3].stock
+	// 				};
+	// 			});
+	// 			res.json(a);
+	// 		}
+	// 	}
+	// );
 	
 });
 
