5fa96e64e072bce87e1af9c92f86b966d4c1fe74
YaburovVV <Vladimir.Yaburov@gmail.com>
2017-10-19 02:28:14 +0300

    cutting json

--- app.js
+++ app.js
@@ -11,26 +11,41 @@ app.get("/getMySclad", function (req, res) {
 	const uriApi = config.get("uriApi");
 	const service = "report/stock/bystore?offset=0&limit=10";
 	const uri = "https://" + user + ":" + pass + "@" + uriApi + service;
-	
-	
-	// fetch('https://api.github.com/users/github')
-	// 	.then(function(res) {
-	// 		return res.json();
-	// 	}).then(function(json) {
-	// 		res.json(json);
-	// 	console.log(json);
-	// });
-	//
+
 	fetch(uri)
-		.then(function(data) {
-			// console.log(data.text());
-			return data.json()
-		})
-		.then(function (asd) {
-			res.json(asd);
+	.then(function (data) {
+		return data.json()
+	})
+	.then(function (data) {
+		console.log(data);
+		var a = data.rows.map(function (el) {
+			return {
+				// id: /\/([\w-]*)\?/g.exec(el.meta.href)[1],
+				// uuid: /=([\w-]*)$/g.exec(el.meta.uuidHref)[1],
+				// href: el.meta.href,
+
+				stock0Name: el.stockByStore[0].name,
+				// stock0Id: /\/([\w-]*)$/g.exec(el.stockByStore[0].meta.href)[1],
+				stock0Value: el.stockByStore[0].stock,
+
+				stock1Name: el.stockByStore[1].name,
+				// stock1Id: /\/([\w-]*)$/g.exec(el.stockByStore[1].meta.href)[1],
+				stock1Value: el.stockByStore[1].stock,
+
+				stock2Name: el.stockByStore[2].name,
+				// stock2Id: /\/([\w-]*)$/g.exec(el.stockByStore[2].meta.href)[1],
+				stock2Value: el.stockByStore[2].stock,
+
+				stock3Name: el.stockByStore[3].name,
+				// stock3Id: /\/([\w-]*)$/g.exec(el.stockByStore[3].meta.href)[1],
+				stock3Value: el.stockByStore[3].stock
+			};
 		});
+
+		res.json(a);
+	});
 	// .then((json)=> res.json(json));
-	
+
 	// request({
 	//         uri: uri,
 	//         method: "GET"
@@ -73,5 +88,5 @@ app.get("/getMySclad", function (req, res) {
 });
 
 app.listen(3000, function () {
-	console.log("listening port 3000");
+    console.log("listening port 3000");
 });
